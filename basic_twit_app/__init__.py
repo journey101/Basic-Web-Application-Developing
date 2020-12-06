2d6845a8c5d12de662968efe64d5188f8698c93f
hyeonjae kim <hyeonjaekim@hyeonjaeui-MacBook.local>
2020-12-06 23:04:55 +0900

    update app

--- basic_twit_app/__init__.py
+++ basic_twit_app/__init__.py
@@ -8,7 +8,8 @@ from basic_twit_app.models import db, migrate
 # DATABASE_URI = f"postgres://abilwzqo:P39FFfs888U_dH0IV9T9p9hK1wsBSQyk@john.db.elephantsql.com:5432/abilwzqo"
 
 # DATABASE_URI = f"postgres://cdfpqaqk:NYBpbU95K63_YL5CgvBCU77hkVha-mA8@john.db.elephantsql.com:5432/cdfpqaqk"
-DATABASE_URI = "sqlite:///twitter.sqlite3"
+# DATABASE_URI = "sqlite:///twitter.sqlite3"
+DATABASE_URI = "postgres://cvzxsdflogngqw:4b1668e1635061d065ce10f7309f8c5a17ab5fa39353758bcc27691124b8c0a0@ec2-52-206-44-27.compute-1.amazonaws.com:5432/d899d99bbdlr5u"
 
 def create_app():
     app = Flask(__name__)
